0cf246b2996b29c59d9e60dbc04ed8ad2019ddae
Mat <mmmateuszmichalski@gmail.com>
2023-09-19 10:31:22 +0200

    image and link models added

--- api_image_task/api_image/models.py
+++ api_image_task/api_image/models.py
@@ -1,21 +1,59 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-class ImageHeigth(models.Model):
-
+class ThumbnailDimentions(models.Model):
     height = models.IntegerField()
+    width = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.height
+
 
 class AccountTier(models.Model):
     name = models.CharField(max_length=64)
     orginal_image_link = models.BooleanField(default=False)
     time_limited_link = models.BooleanField(default=False)
-    image_size = models.ManyToManyField(ImageHeigth)
+    image_size = models.ManyToManyField(ThumbnailDimentions, related_name="size")
 
     def __str__(self):
         return self.name
 
-class ImagexAccount(models.Model):
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    account_type = models.OneToOneField(AccountTier, on_delete=models.DO_NOTHING)
+class ImagexAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user")
+    account_type = models.OneToOneField(
+        AccountTier, on_delete=models.DO_NOTHING, related_name="account_type"
+    )
+
+    def __str__(self):
+        return f"Owner: {self.user} Account type: {self.account_type}."
+
+
+class ImageModel(models.Model):
+    add_time = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=128)
+    author = models.ForeignKey(ImagexAccount, on_delete=models.CASCADE)
+    upload_image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return f" Image title: {self.title} by {self.author} added at {self.add_time}."
+
+
+class ExpiringLinks(models.Model):
+    add_time = models.DateTimeField(auto_now_add=True)
+    image = models.ForeignKey(ImageModel, on_delete=models.CASCADE)
+    time_to_expire = models.IntegerField(
+        validators=[
+            MinValueValidator(300, message="Min sec value is 300"),
+            MaxValueValidator(30000, message="Max sec value is 30000"),
+        ]
+    )
+    owner = models.ForeignKey(
+        ImagexAccount, on_delete=models.CASCADE, related_name="owner"
+    )
+
+    def __str__(self):
+        return f"Line expire in: {self.time_to_expire} sec, owner: {self.owner}."
+
--- api_image_task/api_image_task/settings.py
+++ api_image_task/api_image_task/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api_image"
+    "api_image",
 ]
 
 MIDDLEWARE = [
@@ -114,3 +114,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
